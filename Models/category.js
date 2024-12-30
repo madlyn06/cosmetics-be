c6c8a72fd19941229c278facfcc475ce24d81214
madlyn03 <hongnmp@gmail.com>
2024-12-30 10:45:36 +0700

    update code

--- Models/category.js
+++ Models/category.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
 
 var schema = new mongoose.Schema({
-  category: String
+  category: String,
+  gender: String
 });
 
 var Category = mongoose.model("Category", schema, "category");
